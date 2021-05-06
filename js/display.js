1e006bdf562a34934c9a00651acd77a19f84f92c
kyupkyup <rudduqdl@naver.com>
2021-05-06 15:23:04 +0900

    fix: js fix

--- js/display.js
+++ js/display.js
@@ -1,29 +1,3 @@
-const detailList = document.querySelector('.detail-info');
-
-function detailClickHandler(e) {
-  let elem = e.target;
-  while (!elem.classList.contains('detail-info__title')) {
-    elem = elem.parentNode;
-
-    if (elem.nodeName == 'BODY') {
-      elem = null;
-      return;
-    }
-  }
-
-  if (prevDep2List == null) {
-    elem.parentNode.classList.add('is-open');
-  } else if (prevDep2List == elem.parentNode) {
-    elem.parentNode.classList.remove('is-open');
-    prevDep2List = null;
-    return;
-  } else {
-    prevDep2List.classList.remove('is-open');
-    elem.parentNode.classList.add('is-open');
-  }
-  prevDep2List = elem.parentNode;
-}
-
 const ing = document.getElementById('ongoingTab');
 const end = document.getElementById('finishedTab');
 const listIng = document.getElementById('ongoing');
