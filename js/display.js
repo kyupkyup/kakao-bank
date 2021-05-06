dea067deef5ccab5e082340337fc69bdc4cc86b4
DongGunYoon <52520194+DongGunYoon@users.noreply.github.com>
2021-05-06 13:27:30 +0900

    fix: Change tab panel id in display.js (#13)
    - 이벤트 페이지 탭패널 javascript 적용(id, aria속성)
    - main 영역 상단 패딩 추가
    - 메인페이지에 gototop 버튼 추가

--- js/display.js
+++ js/display.js
@@ -16,17 +16,21 @@ for (let idx = 0; idx < list.length; idx++) {
   });
 }
 
-// const ing = document.getElementById('진행 버튼 아이디');
-// const end = document.getElementById('종료 버튼 아이디');
-// const listIng = document.getElementById('진행 컨테이너 아이디');
-// const listEnd = document.getElementById('종료 컨테이너 아이디');
+const ing = document.getElementById('ongoingTab');
+const end = document.getElementById('finishedTab');
+const listIng = document.getElementById('ongoing');
+const listEnd = document.getElementById('finished');
 
-// // is--invalid 에 display:none 추가
-// ing.addEventListener('click', function () {
-//   listEnd.className += ' is--invalid';
-//   listIng.className = '';
-// });
-// end.addEventListener('click', function () {
-//   listIng.className += ' is--invalid';
-//   listEnd.className = '';
-// });
+// is--invalid 에 display:none 추가
+ing.addEventListener('click', function () {
+  listEnd.className += ' is--invalid';
+  listIng.className = '';
+  ing.setAttribute('aria-selected', 'true');
+  end.setAttribute('aria-selected', 'false');
+});
+end.addEventListener('click', function () {
+  listIng.className += ' is--invalid';
+  listEnd.className = '';
+  end.setAttribute('aria-selected', 'true');
+  ing.setAttribute('aria-selected', 'false');
+});
